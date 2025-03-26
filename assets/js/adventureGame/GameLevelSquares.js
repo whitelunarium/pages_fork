3439623a64656c0248cd4a85222dc41d8124da1f
jm1021 <jmort1021@gmail.com>
2025-03-25 20:27:49 -0700

    add parallax

--- assets/js/adventureGame/GameLevelSquares.js
+++ assets/js/adventureGame/GameLevelSquares.js
@@ -1,12 +1,15 @@
 // To build GameLevels, each contains GameObjects from below imports
 import Background from './Background.js';
+import BackgroundParallax from './BackgroundParallax.js';
 import Player from './Player.js';
 
 // Minimal Definition
 class GameLevelSquares {
-  constructor(path) {
+  constructor(gameEnv) {
+    let path = gameEnv.path;
     this.classes = [      
       { class: Background, data: {} },
+      { class: BackgroundParallax, data: {src:  path + "/images/platformer/backgrounds/snowfall.png", velocity: 1} },
       { class: Player, data: {} }, 
     ];
   }
