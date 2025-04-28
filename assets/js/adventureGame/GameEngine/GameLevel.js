f422bf8d895dacb23ee95e7a51709e0a2b76161b
EvanSvetina <evanthehedgehog43@gmail.com>
2025-04-28 13:50:03 -0700

    small tweaks to make game run again

--- assets/js/adventureGame/GameEngine/GameLevel.js
+++ assets/js/adventureGame/GameEngine/GameLevel.js
@@ -1,4 +1,3 @@
-// GameLevel.js
 import GameEnv from "./GameEnv.js"
 
 class GameLevel {
@@ -57,78 +56,55 @@ class GameLevel {
       console.error('Error creating game level:', error);
       this.continue = false;
     }
-      for (let gameObjectClass of this.gameObjectClasses) {
-        if (!gameObjectClass.data) gameObjectClass.data = {}
-        
-        // Check if class property is a constructor
-        if (typeof gameObjectClass.class !== 'function') {
-          console.error('GameObject class is not a constructor', gameObjectClass);
-          continue; // Skip this object but don't crash the level
-        }
-        
-        let gameObject = new gameObjectClass.class(gameObjectClass.data, this.gameEnv)
-        this.gameEnv.gameObjects.push(gameObject)
-      }
-
-    if (typeof this.gameLevel.initialize === "function") {
-      this.gameLevel.initialize()
-
-    }
-
-      window.addEventListener("resize", this.resize.bind(this))
-      
-    } catch (error) {
-      console.error('Error creating game level:', error);
-      this.continue = false;
-    }
   }
 
   destroy() {
-  // Check if gameLevel exists before trying to call destroy
-  if (this.gameLevel && typeof this.gameLevel.destroy === "function") {
-    this.gameLevel.destroy();
-  }
+    // Check if gameLevel exists before trying to call destroy
+    if (this.gameLevel && typeof this.gameLevel.destroy === "function") {
+      this.gameLevel.destroy();
+    }
 
-  // Safely remove all game objects
-  if (this.gameEnv && this.gameEnv.gameObjects) {
-    for (let index = this.gameEnv.gameObjects.length - 1; index >= 0; index--) {
-      if (this.gameEnv.gameObjects[index]) {
-        this.gameEnv.gameObjects[index].destroy();
+    // Safely remove all game objects
+    if (this.gameEnv && this.gameEnv.gameObjects) {
+      for (let index = this.gameEnv.gameObjects.length - 1; index >= 0; index--) {
+        if (this.gameEnv.gameObjects[index]) {
+          this.gameEnv.gameObjects[index].destroy();
+        }
       }
     }
+    
+    // Remove event listener
+    window.removeEventListener("resize", this.resize.bind(this));
   }
-  
-  // Remove event listener
-  window.removeEventListener("resize", this.resize.bind(this));
-}
 
-update() {
-  if (!this.gameEnv) return;
-  
-  this.gameEnv.clear();
+  update() {
+    if (!this.gameEnv) return;
+    
+    this.gameEnv.clear();
 
-  if (this.gameEnv.gameObjects) {
-    for (let gameObject of this.gameEnv.gameObjects) {
-      if (gameObject) {
-        gameObject.update();
+    if (this.gameEnv.gameObjects) {
+      for (let gameObject of this.gameEnv.gameObjects) {
+        if (gameObject) {
+          gameObject.update();
+        }
       }
+    }
+
+    if (this.gameLevel && typeof this.gameLevel.update === "function") {
+      this.gameLevel.update();
     }
   }
 
-  if (this.gameLevel && typeof this.gameLevel.update === "function") {
-    this.gameLevel.update();
-  }
-}
-
-resize() {
-  if (!this.gameEnv) return;
-  
-  this.gameEnv.resize();
-  
-  if (this.gameEnv.gameObjects) {
-    for (let gameObject of this.gameEnv.gameObjects) {
-      if (gameObject) {
-        gameObject.resize();
+  resize() {
+    if (!this.gameEnv) return;
+    
+    this.gameEnv.resize();
+    
+    if (this.gameEnv.gameObjects) {
+      for (let gameObject of this.gameEnv.gameObjects) {
+        if (gameObject) {
+          gameObject.resize();
+        }
       }
     }
   }
