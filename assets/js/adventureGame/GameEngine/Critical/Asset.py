f2d504672c6ccd6be6568d1dc863f43c7aa8d611
EvanSvetina <evanthehedgehog43@gmail.com>
2025-05-05 14:07:48 -0700

    gorg

---
+++ assets/js/adventureGame/GameEngine/Critical/Asset.py
@@ -0,0 +1,51 @@
+# INTRO
+# Oh yeah baby, it's time for the real bar builders to lay dow the sickest CSSE2 beat you've ever seen!!!!
+
+# Verse 1
+# Hey there kiddos, i'm Mr. Mort
+# Better get to working on the 2025 port
+# Set up those tools, and don't forget node!
+# Better get Slacking, lemme hear you say 
+# CODE CODE CODE
+
+# Chorus
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
