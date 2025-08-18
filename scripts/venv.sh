d01ba6398fc245efb12985cdef1f2d7c7194f083
jm1021 <jmort1021@gmail.com>
2025-08-17 18:46:44 -0700

    remove activate from venv

--- scripts/venv.sh
+++ scripts/venv.sh
@@ -9,9 +9,6 @@ if [ ! -d "venv" ]; then
     python -m venv venv
 fi
 
-# Activate the virtual environment
-source venv/bin/activate
-
 # Install the required Python packages
 pip install -r requirements.txt
 
