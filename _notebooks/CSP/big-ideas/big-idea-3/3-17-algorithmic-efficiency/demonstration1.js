50e44a32f697818028aaf40b6bdbe825b3656c54
Aditya Srivastava <adi4utube@gmail.com>
2025-10-08 22:28:45 -0700

    Delete _notebooks/CSP/big-ideas/big-idea-3/3-17-algorithmic-efficiency/demonstration1.js

--- _notebooks/CSP/big-ideas/big-idea-3/3-17-algorithmic-efficiency/demonstration1.js
+++
@@ -1,9 +0,0 @@
-const canvas = document.getElementById("display1");
-const ctx = canvas.getContext("2d");
-
-function start(){
-    console.log("start");
-    ctx.fillStyle = "red";
-    ctx.fillRect(50, 50, 100, 100);
-}
-
