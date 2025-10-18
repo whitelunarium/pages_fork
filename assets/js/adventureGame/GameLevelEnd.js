e9290b6040c664c21b90a9c48ce6b816f7d10f23
jm1021 <jmort1021@gmail.com>
2025-10-18 07:28:46 -0700

    Steve.png to steve.png

--- assets/js/adventureGame/GameLevelEnd.js
+++ assets/js/adventureGame/GameLevelEnd.js
@@ -52,7 +52,7 @@ class GameLevelEnd {
         zIndex: 5  // Higher z-index to appear above parallax
     };
     
-    const sprite_src_chillguy = path + "/images/gamify/Steve.png";
+    const sprite_src_chillguy = path + "/images/gamify/steve.png";
     const CHILLGUY_SCALE_FACTOR = 7;
     const sprite_data_chillguy = {
         id: 'Steve',
